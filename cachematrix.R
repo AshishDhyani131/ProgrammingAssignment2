6a343535b7ec1ab496521e8eec44df1dc79db926
Ashish Dhyani <ashishdhyani100@gmail.com>
2020-12-31 11:41:22 +0530

    Added code in functions of cachematrix.R

--- cachematrix.R
+++ cachematrix.R
@@ -4,6 +4,18 @@
 ## Write a short comment describing this function
 
 makeCacheMatrix <- function(x = matrix()) {
+    inverse<-NULL
+    setMatrix<-function(y){
+        x<<-y
+        inverse<-NULL
+    }
+    getMatrix<-function()x
+    setInverse<-function(Inverse){
+        inverse<<-Inverse
+    }
+    getInverse<-function()inverse
+    list(setMatrix = setMatrix,getMatrix=getMatrix,
+         setInverse=setInverse,getInverse=getInverse)
 
 }
 
@@ -12,4 +24,13 @@ makeCacheMatrix <- function(x = matrix()) {
 
 cacheSolve <- function(x, ...) {
         ## Return a matrix that is the inverse of 'x'
+    inverse<-x$getInverse()
+    if(!is.null(inverse)){
+        print("Loading inverse")
+        return(inverse)
+    }
+    data<-x$getMatrix
+    inverse<-solve(data)
+    x$setInverse(inverse)
+    inverse
 }
